27c07e4ebb5fcef6923c180ee8d8abd9322e584b
DE4DDNE3 <deaddnee@proton.me>
2025-03-02 13:52:16 -0600

    got audio working :D

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from http.server import SimpleHTTPRequestHandler, test
-import pygame
-test(SimpleHTTPRequestHandler)
+#prequesites
+test10mb = "./mp3/10mbtestr.mp3"
+import	pygame
+pygame.init()
+pygame.mixer.init()
+
+# i don't like running the server every test..
+testque = input("test? (y/n): ")
+if testque == "n":
+	from http.server import SimpleHTTPRequestHandler, test
+	test(SimpleHTTPRequestHandler)
+else:
+	print("Server init skipped")
+
+pygame.mixer.music.load(test10mb)
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy():
+	pygame.time.Clock().tick(10)
+pygame.mixer.music.stop()
+pygame.mixer.quit()
+pygame.quit()
--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@ clear
 base64 -d asciime.txt
 # starting py venv & script
 source .venv/bin/activate
+python3 main.py
+# end of script
